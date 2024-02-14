f80d3f4af5240fba7d988435730d4c62a4fd5e81
Jere Janhunen <djeja003@edu.xamk.fi>
2024-02-14 11:14:29 +0200

    Add project files.

---
+++ DesignPrinciplesAssignment/Program.cs
@@ -0,0 +1,18 @@
+﻿using static DesignPrinciplesAssignment.User;
+
+namespace DesignPrinciplesAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Design principles assignment: See assignment.cs files for assignments");
+
+            //SRP
+            User user = new User("John", "test");
+            UserRepository userRepository = new UserRepository();
+
+            userRepository.SaveUser(user);
+        }
+    }
+}
